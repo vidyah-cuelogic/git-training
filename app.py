66e6684162bb058c1af61cd2aaf49752a9d7d996
vidyah-cuelogic <vidya.hulwan@cuelogic.com>
2017-12-21 17:09:18 +0530

    set environment variable on production server

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from jinja2 import Environment, FileSystemLoader
 env = Environment(loader=FileSystemLoader('templates'))
 
+
 class HelloWorld(object):
     @cherrypy.expose
     def index(self):
@@ -14,10 +15,10 @@ config = {
         'server.socket_host': '0.0.0.0',
         'server.socket_port': int(os.environ.get('PORT', 5000)),
     },
-    '/assets': {
+    '/static': {
         'tools.staticdir.root': os.path.dirname(os.path.abspath(__file__)),
         'tools.staticdir.on': True,
-        'tools.staticdir.dir': 'assets',
+        'tools.staticdir.dir': 'static',
     }
 }
 
